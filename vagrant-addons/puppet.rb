aee15f3ec7273cdd7494cafbb4ce826376012522
Jan Vansteenkiste <jan@vstone.eu>
2011-10-10 15:33:49 +0200

    Add pretty vagrant module structure

--- vagrant-addons/puppet.rb
+++ vagrant-addons/puppet.rb
@@ -1,15 +1,18 @@
-class PuppetCommand < Vagrant::Command::GroupBase
-  register "puppet", "Do all sorts of fun stuff with puppet"
+module Vagrant
+  module Command
+    class PuppetCommand < Vagrant::Command::GroupBase
+      register "puppet", "Do all sorts of fun stuff with puppet"
 
-  source_root File.expand_path("../templates", __FILE__)
+      source_root File.expand_path("../templates", __FILE__)
 
-  desc "module_add NAME", "Do stuff with modules"
-  def module_add(name)
-    env.ui.info "Create puppet module structure for new module '#{name}'"
-    empty_directory "modules/#{name}/templates"
-    empty_directory "modules/#{name}/manifests"
-    opts = { :name => "#{name}" }
-    template('module_init.tt', "modules/#{name}/manifests/init.pp", opts)
+      desc "module_add NAME", "Do stuff with modules"
+      def module_add(name)
+        env.ui.info "Create puppet module structure for new module '#{name}'"
+        empty_directory "modules/#{name}/templates"
+        empty_directory "modules/#{name}/manifests"
+        opts = { :name => "#{name}" }
+        template('module_init.tt', "modules/#{name}/manifests/init.pp", opts)
+      end
+    end
   end
-
 end
